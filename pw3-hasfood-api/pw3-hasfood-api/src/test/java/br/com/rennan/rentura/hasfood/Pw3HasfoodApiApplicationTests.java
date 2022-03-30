5c8b6fbf169400b27b88cfa63de0347f5b6802fe
Rennan <email@test.com>
2022-03-30 13:37:25 -0300

    criacao do projeto

---
+++ pw3-hasfood-api/pw3-hasfood-api/src/test/java/br/com/rennan/rentura/hasfood/Pw3HasfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rennan.rentura.hasfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw3HasfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
